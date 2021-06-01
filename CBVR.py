a9ae49e1a6d52858f23a0a794444b75c3a2b315b
Ahmed Yehia <ahmedyehia.edu@gmail.com>
2021-06-01 17:47:49 +0200

    implementing keyframe extraction function

---
+++ CBVR.py
@@ -0,0 +1,37 @@
+from cv2 import cv2
+import numpy as np
+
+def keyframe(name,threshold=0.3):
+    ''' 
+    parameters
+        name: string path of processed video
+        threshold: float value control the threshold of comparison bet. frames
+    extract keyframes from a video
+    return: np.array of keyframes 
+    '''
+
+    cap = cv2.VideoCapture(name)
+    frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))-1
+    frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    ret,current = cap.read()
+    prev = current
+    prevHist = np.array([cv2.calcHist([prev],[i],None,[256],[0,256]) for i in [0,1,2]])
+    prevHist = prevHist / (frameHeight*frameWidth)
+    keyframes = [prevHist]
+
+    for dummy in range(1,frameCount):
+        if ret:
+            ret, current = cap.read()
+            currentHist = np.array([cv2.calcHist([current],[i],None,[256],[0,256]) for i in [0,1,2]])
+            currentHist = currentHist / (frameHeight*frameWidth)
+            dif = np.sum(np.abs(prevHist-currentHist))/3
+            if dif > threshold:
+                keyframes.append(currentHist)
+            prev = current
+            prevHist = currentHist
+
+    cap.release()
+    return np.array(keyframes)
+
